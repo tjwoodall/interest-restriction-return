d6f2f9ec3500e89635761cdb3d196b9ffc280465
lmugnai <11977337+lmugnai@users.noreply.github.com>
2026-03-06 17:07:06 +0000

    Update deps + plugins to address security vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val bootstrapPlayVersion = "10.1.0"
+  private lazy val bootstrapPlayVersion = "10.7.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
